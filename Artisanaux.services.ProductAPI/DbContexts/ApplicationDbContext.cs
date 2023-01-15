13b3a9938458d3a3cc1df4227a21b4b147bb8686
Administrator <Administrator@LAP-5CG7030T84>
2023-01-15 08:22:45 +0000

    Add project files.

---
+++ Artisanaux.services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Artisanaux.services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Artisanaux.services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+         
+    }
+}
---
+++ Artisanaux.services.ProductAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Artisanaux.services.ProductAPI.Models
+{
+   // [Table("Products")]
+    public class Product
+    {   
+        [Key]
+        public int ProductId { get; set; }
+        [Required, StringLength(25)]
+        public string ProductName { get; set; }
+        [Range(1, 1000)]
+        public Double Price { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
